533ff6112f93f0e32720e5a68c80c24dea580fb1
karthikhariharan <karthikhariharan13@gmail.com>
2015-10-08 00:34:12 -0500

    Config file template for sensitive data

---
+++ custom_modules/default_config.js
@@ -0,0 +1,7 @@
+// Populate this file with your twitter credentials and save the file as config.js in the same directory as this file
+var twitter_credentials = {    
+    consumer_key: '',
+    consumer_secret: '',
+    access_token_key: '',
+    access_token_secret: ''
+}
